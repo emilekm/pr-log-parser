c7882609f3a92019f586b40784d390d85bb4576c
Emil Maruszczak <emilekm@gmail.com>
2016-09-15 15:21:00 +0000

    Better error handling and tests for parseRecord

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -4,11 +4,12 @@ const expect = chai.expect;
 var utils = require('../lib/utils');
 
 var records = [
-    ''
-]
+    '[2016-04-21 19:01:31] d41d8cd98f00b204e9800998ecf8427e player 192.168.0.1',
+    '[2016-04-21 19:01:31] d41d8cd98f00b204e9800998ecf8427e Clan player 192.168.0.1'
+];
 
 describe('utils.parseRecord', function() {
-    
+
     it('should return error if no arguments passed in', function() {
         expect(utils.parseRecord).to.throw(Error, 'No arguments were passed');
     });
@@ -29,8 +30,22 @@ describe('utils.parseRecord', function() {
         }).to.throw(Error, 'Record was not passed');
     });
 
-    it('should return array with parsed log records', function() {
+    it('should return object with parsed log record if record and callback are passed(record without clan-tag)', function() {
+        expect(utils.parseRecord(records[0], function(err, record) {
+            if (err) {
+                throw err;
+            }
+            return record;
+        })).to.contain.all.keys(['name', 'time', 'hash', 'ip']);
+    });
 
+    it('should return object with parsed log record if record and callback are passed(record with clan-tag)', function() {
+        expect(utils.parseRecord(records[1], function(err, record) {
+            if (err) {
+                throw err;
+            }
+            return record;
+        })).to.contain.all.keys(['name', 'time', 'hash', 'ip', 'tag']);
     });
 
 });
