9049b874625c69aa74822dd5c6e50896a2cbad45
Emil Maruszczak <emilekm@gmail.com>
2016-09-15 16:26:32 +0000

    Working parseRecord with tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -5,7 +5,7 @@ var utils = require('../lib/utils');
 
 var records = [
     '[2016-04-21 19:01:31] d41d8cd98f00b204e9800998ecf8427e player 192.168.0.1',
-    '[2016-04-21 19:01:31] d41d8cd98f00b204e9800998ecf8427e Clan player 192.168.0.1'
+    '[2016-04-21 19:02:31] d41d8cd98f00b204e9800998ecf8427e Clan player 192.168.0.1'
 ];
 
 describe('utils.parseRecord', function() {
@@ -30,22 +30,25 @@ describe('utils.parseRecord', function() {
         }).to.throw(Error, 'Record was not passed');
     });
 
-    it('should return object with parsed log record if record and callback are passed(record without clan-tag)', function() {
-        expect(utils.parseRecord(records[0], function(err, record) {
+    it('should return object with parsed log record if record and callback are passed(record without clan-tag)', function(done) {
+        utils.parseRecord(records[0], function(err, record) {
             if (err) {
-                throw err;
+                done(err);
             }
-            return record;
-        })).to.equal({name: 'player', 'hash': 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(2016, 3, 21, 19, 01, 31)});
+
+            expect(record).to.eql({name: 'player', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(2016, 3, 21, 19, 01, 31)});
+            done();
+        });
     });
 
-    it('should return object with parsed log record if record and callback are passed(record with clan-tag)', function() {
-        expect(utils.parseRecord(records[1], function(err, record) {
+    it('should return object with parsed log record if record and callback are passed(record with clan-tag)', function(done) {
+        utils.parseRecord(records[1], function(err, record) {
             if (err) {
-                throw err;
+                done(err);
             }
-            return record;
-        })).to.equal({name: 'player', tag: 'Clan', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(2016, 3, 21, 19, 01, 31)});
+            expect(record).to.eql({name: 'player', tag: 'Clan', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(2016, 3, 21, 19, 02, 31)});
+            done();
+        });
     });
 
 });
