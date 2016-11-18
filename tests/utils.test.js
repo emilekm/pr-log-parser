5029cd77edf5982d02c26febc0479a1c2573ac88
Emil Maruszczak <emilekm@gmail.com>
2016-11-18 15:54:58 +0100

    Polish tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -3,10 +3,11 @@ const expect = chai.expect;
 
 var utils = require('../lib/utils');
 
-var records = [
+var testRecords = [
     '[2016-04-21 19:01:31] d41d8cd98f00b204e9800998ecf8427e player 192.168.0.1',
     '[2016-04-21 19:02:31] d41d8cd98f00b204e9800998ecf8427e Clan player 192.168.0.1'
 ];
+var testStdout = '[2016-04-21 19:01:31] d41d8cd98f00b204e9800998ecf8427e player 192.168.0.1\n[2016-04-21 19:02:31] d41d8cd98f00b204e9800998ecf8427e Clan player 192.168.0.1';
 
 describe('utils.parseHashRecord', function() {
 
@@ -14,7 +15,7 @@ describe('utils.parseHashRecord', function() {
         expect(utils.parseHashRecord).to.throw(Error, 'No arguments were passed');
     });
 
-    it('should throw error if only callback function is passed', function() {
+    it('should throw error if something else than string is passed', function() {
         expect(function() {
             utils.parseHashRecord([]);
         }).to.throw(Error, 'Record must be a string');
@@ -30,27 +31,30 @@ describe('utils.parseHashRecord', function() {
         }).to.throw(Error, 'Record was not passed');
     });
 
-    it('should throw error if no callback is passed', function() {
-        expect(utils.parseHashRecord(records[0])).to.eql({name: 'player', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(2016, 3, 21, 19, 01, 31)});
+    var player = {name: 'player', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(Date.UTC(2016, 3, 21, 19, 01, 31))};
+    var clan_player = {name: 'player', tag: 'Clan', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(Date.UTC(2016, 3, 21, 19, 02, 31))};
+
+    it('should return record object if no callback is passed', function() {
+        expect(utils.parseHashRecord(testRecords[0])).to.eql(player);
     });
 
     it('should return object with parsed log record if record and callback are passed(record without clan-tag)', function(done) {
-        utils.parseHashRecord(records[0], function(err, record) {
+        utils.parseHashRecord(testRecords[0], function(err, record) {
             if (err) {
                 done(err);
             }
 
-            expect(record).to.eql({name: 'player', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(2016, 3, 21, 19, 01, 31)});
+            expect(record).to.eql(player);
             done();
         });
     });
 
     it('should return object with parsed log record if record and callback are passed(record with clan-tag)', function(done) {
-        utils.parseHashRecord(records[1], function(err, record) {
+        utils.parseHashRecord(testRecords[1], function(err, record) {
             if (err) {
                 done(err);
             }
-            expect(record).to.eql({name: 'player', tag: 'Clan', hash: 'd41d8cd98f00b204e9800998ecf8427e', ip: '192.168.0.1', time: new Date(2016, 3, 21, 19, 02, 31)});
+            expect(record).to.eql(clan_player);
             done();
         });
     });
